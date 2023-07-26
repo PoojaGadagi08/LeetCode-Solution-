1438970a3a68572ad4984699a8420eddc82dc80d
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-26 15:41:00 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -33,3 +33,24 @@ Output: [0,1]
         return new int[] {};
     }
 }
+
+
+
+
+
+class Solution {
+    public int []twoSum(int[] arr, int target) {
+        int n=arr.length;
+         HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                return "YES";
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return "NO";
+    }
+}
